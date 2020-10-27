f9edfe521b4cb3532428711f735d0b3f6ccad835
ksm9606 <54825978+ksm9606@users.noreply.github.com>
2020-10-27 12:58:12 +0900

    JS: Home contact button

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Navbar 올라가면 투명 내려가면 핑크
+// Navbar 올라가면 투명, 내려가면 핑크
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height
 
@@ -9,13 +9,13 @@ document.addEventListener('scroll', ()=>{
     console.log(`navbarHeight : ${navbarHeight}`);
 
     if(window.scrollY > navbarHeight){
-        navbar.classList.add('navbar--dark')
+        navbar.classList.add('navbar--dark');
     }else{
         navbar.classList.remove('navbar--dark')
     }
 })
 
-// Navbar menu scrolling
+// Navbar menu scrolling 
 const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener('click', (event)=>{
     const target = event.target;
@@ -24,7 +24,29 @@ navbarMenu.addEventListener('click', (event)=>{
         return;
     }
 
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior:"smooth"});
+    scrollIntoView(link);
+
+    // 이렇게도 사용가능
+    // console.log(event.target.dataset.link);
+    // const scrollTo = document.querySelector(link);
+    // scrollTo.scrollIntoView({behavior:"smooth"});
 })
+
+// Home Contact me scrolling
+
+// 이렇게도 사용가능
+// const homeContactBtn = document.querySelector(".home__contact");
+// homeContactBtn.addEventListener('click',()=>{
+//     const scrollTo = document.querySelector("#contact");
+//     scrollTo.scrollIntoView({behavior:"smooth"});
+// })
+
+const homeContactBtn = document.querySelector(".home__contact");
+homeContactBtn.addEventListener('click',()=>{
+    scrollIntoView('#contact');
+})
+
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior:"smooth"});
+}
